723e1dbad7deb7e17d0a6635c4402d14dd3d1461
Jedrzej Winnicki <jwinnicki2@gmail.com>
2019-12-28 19:31:45 +0100

    Updates

---
+++ precache-manifest.805317c5c187100b2921687958e7e5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76faeb9ff1708b216754ee562dd8997",
+    "url": "/YT-Video-Finder/index.html"
+  },
+  {
+    "revision": "4af41ac9e9d75d7c591e",
+    "url": "/YT-Video-Finder/static/css/main.2051e254.chunk.css"
+  },
+  {
+    "revision": "469182ad03a4148d4b0c",
+    "url": "/YT-Video-Finder/static/js/2.d02cf4df.chunk.js"
+  },
+  {
+    "revision": "4af41ac9e9d75d7c591e",
+    "url": "/YT-Video-Finder/static/js/main.e9ce0fd6.chunk.js"
+  },
+  {
+    "revision": "6525c937f48c0154001d",
+    "url": "/YT-Video-Finder/static/js/runtime-main.96712587.js"
+  },
+  {
+    "revision": "ac940ccef6f2e1c994cd23e6a179c5b0",
+    "url": "/YT-Video-Finder/static/media/sprite.ac940cce.svg"
+  }
+]);
